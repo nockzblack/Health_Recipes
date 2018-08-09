87b086ad588a4fb66721e46a67ffb7758d0d0f70
Fernando Benavides <nockzblack@gmail.com>
2018-08-09 17:39:03 -0500

    Initial Commit

---
+++ Health_Recipes/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Health_Recipes
+//
+//  Created by Fernando Benavides Rodriguez on 8/9/18.
+//  Copyright © 2018 Nockzblack. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
