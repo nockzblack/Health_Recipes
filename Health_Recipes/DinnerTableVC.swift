44c8591d563e99fc4a579c1bf7efcdeda3486306
Fernando Javier Benavides Rodríguez <ferbenavides98@hotmail.com>
2018-12-20 07:14:59 -0600

    Now it is possible to eliminate items/data from BreakfastTableVC and DinnerTableVC

--- Health_Recipes/DinnerTableVC.swift
+++ Health_Recipes/DinnerTableVC.swift
@@ -76,36 +76,20 @@ class DinnerTableVC: UITableViewController, AddRecipeDelegate {
         self.performSegue(withIdentifier: "RecipeSegue", sender: recipe)
     }
     
-    
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            dinnerList.remove(at: indexPath.row)
+            tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            tableView.endUpdates()
+            storeData()
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
